7462028b6ef40238229684f18eea584b074e2168
Matthias Urlichs <matthias@urlichs.de>
2023-05-25 21:24:31 +0200

    Deploy-Befehl

--- conf.py
+++ conf.py
@@ -674,11 +674,12 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete --delete-after output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+     'default': [    
+         "rsync -rav --delete output/. solar@dispatch.vm.smurf.noris.de:/var/www/solar/.",
+     ]
+}
+
 
 # github_deploy configuration
 # For more details, read the manual:
