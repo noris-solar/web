b515daa6a049b897e83a8d7efd92348b12d11d57
Matthias Urlichs <matthias@urlichs.de>
2023-05-25 21:28:26 +0200

    favicon

--- conf.py
+++ conf.py
@@ -939,10 +939,12 @@ IMAGE_FOLDERS = {'images': 'images'}
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
 # <link rel="name" href="file" sizes="size"/>
-# FAVICONS = (
-#     ("icon", "/favicon.ico", "16x16"),
-#     ("icon", "/icon_128x128.png", "128x128"),
-# )
+FAVICONS = (
+    #("icon", "/favicon.ico", "16x16"),
+    #("icon", "/favicon_32.png", "32x32"),
+    ("icon", "/favicon_64.png", "64x64"),
+    #("icon", "/favicon_16.png", "16x16"),
+)
 
 # Show teasers (instead of full posts) in indexes? Defaults to False.
 # INDEX_TEASERS = False
